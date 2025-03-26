7735c4ee63a82ac01701f8dba730f0a4b526f35a
Yonela Mazele <mazeleyonela8@gmail.com>
2025-03-27 01:03:04 +0200

    reducer function

--- store.js
+++ store.js
@@ -9,3 +9,20 @@ const RESET = 'RESET';
 const add = () => ({ type: ADD });
 const subtract = () => ({ type: SUBTRACT });
 const reset = () => ({ type: RESET });
+
+// Initial State
+const initialState = { count: 0 };
+
+// Reducer Function
+const counterReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case ADD:
+      return { count: state.count + 1 };
+    case SUBTRACT:
+      return { count: state.count - 1 };
+    case RESET:
+      return { count: 0 };
+    default:
+      return state;
+  }
+}; 
